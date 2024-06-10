bdf3f2542da918051cb92a3c7cf6b31307cb5652
personal <gunwant7272@gmail.com>
2024-06-10 18:20:04 +0530

    auth fixed

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -10,7 +10,24 @@ export const authOptions = {
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET ?? "",
+    session: {
+        jwt: true,
+    },
     adapter: PrismaAdapter(prisma) as Adapter,
+    jwt: {
+        secret: process.env.NEXTAUTH_SECRET,
+        encryption: true,
+    },
     callbacks: {
+        async jwt(token, user) {
+            if (user) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session(session, token) {
+            session.user.id = token.id;
+            return session;
+        },
     },
 }
